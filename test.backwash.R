a986e369231f248bafd32037e71fa02d2208d2f5
Peter Carbonetto <peter.carbonetto@gmail.com>
2018-01-23 08:09:32 -0600

    A few small revisions to test.backwash.R.

--- test.backwash.R
+++ test.backwash.R
@@ -9,14 +9,13 @@ onsex[is.na(onsex)] <- FALSE
 dat$ctl[onsex]      <- FALSE
 
 cat("Running SVA.\n")
-num_sv        <- sva::num.sv(dat = dat$Y,mod = dat$X)
-Y             <- t(dat$Y)
-X             <- dat$X
-control_genes <- dat$ctl
+num_sv <- sva::num.sv(dat = dat$Y,mod = dat$X)
+Y      <- t(dat$Y)
+X      <- dat$X
 
 cat("Running mouthwash.\n")
 r <- system.time(mout <- mouthwash(Y = Y,X = X,k = num_sv))
-cat("Computation took",r["elapsed"],"seconds.\n")
+cat("mouthwash took",r["elapsed"],"seconds.\n")
 
 cat("Running backwash.\n")
 r <- system.time(bout <- backwash(Y = Y,X = X,k = num_sv))
